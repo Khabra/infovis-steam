82ff4712cd311ffc3b4bb84c330684f6917699f6
root <root@VYWsPC.>
2025-09-17 19:40:23 -0300

    Navbar + LandingPage(base) creados, css wonito añadido :D

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        {/* Logo */}
+        <div
+          className="navbar-logo"
+          onClick={() => navigate('/')}
+          style={{ cursor: 'pointer' }}
+        >
+          <img
+            className="navbar-logo-img"
+            src="/BacklogSteamLogo.png"
+            alt="Backlog Logo"
+          />
+        </div>
+
+        <button onClick={() => navigate('/about')}>About Us</button>
+      </div>
+
+      <div className="navbar-links">
+        <button onClick={() => navigate('/backlog')}>Backlog General</button>
+        <button onClick={() => navigate('/generos')}>Géneros</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <>
+    Hola
+    </>
+  );
+}
+
+export default LandingPage;
